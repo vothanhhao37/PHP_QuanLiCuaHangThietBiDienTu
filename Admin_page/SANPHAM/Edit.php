09ab889abf44a4847d9ff6fe7f87ccda4fcf1018
Truong231202 <86698098+Truong231202@users.noreply.github.com>
2023-11-06 16:26:27 +0700

    Update Edit.php

--- Admin_page/SANPHAM/Edit.php
+++ Admin_page/SANPHAM/Edit.php
@@ -1,192 +1,150 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include("../../db_connect.php");
+$maSP = $_GET['maSP'];
+$sql_sanpham = "SELECT TENSP, DONGIA, SOLUONG, MOTA, ANH, TENLOAISP, TENTHUONGHIEU, HEDIEUHANH, sanpham.MATSKT
+FROM ((sanpham join loaisanpham on sanpham.MALOAISP = loaisanpham.MALOAISP) join thuonghieu on
+sanpham.MATH = thuonghieu.MATH) join thongsokythuat on sanpham.MATSKT=thongsokythuat.MATSKT
+WHERE sanpham.MASP = '$maSP'";
+$result = mysqli_query($conn, $sql_sanpham);
+$row = mysqli_fetch_assoc($result);
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+if (isset($_POST["luu"])) {
+        $target_dir = "../../Images/";
+        $target_file = $target_dir . basename($_FILES["Avatar"]["name"]);
+        $check = getimagesize($_FILES["Avatar"]["tmp_name"]);
+        if ($check !== false) {
+                move_uploaded_file($_FILES["Avatar"]["tmp_name"], $target_file);
+                $sql = "UPDATE sanpham SET TENSP = '" . $_POST['TENSP'] . "', DONGIA = '" . $_POST['DONGIA'] . "', 
+                SOLUONG = '" . $_POST['SOLUONG'] . "', MOTA = '" . $_POST['MOTA'] . "', ANH = '" . $_FILES["Avatar"]["name"] . "', 
+                MALOAISP = '" . $_POST['loaisp'] . "', MATH = '" . $_POST['thuonghieu'] . "', MATSKT = '" . $_POST['tskt'] . "'
+                WHERE MASP = '".$maSP."'";
+                $result = mysqli_query($conn, $sql);
+                header('Location:index.php');
+        }
+        ?>
+        <script>
+                window.alert("Tệp ảnh không hợp lệ");
+        </script>
+        <?php
 
-<body>
-    <?php
-    require("../shared/header.php");
-    require("../../db_connect.php")
-    ?>
-
-    <h2 class="heading">Chỉnh sửa thông tin sản phẩm</h2>
-    <form action="" method="post">
-        <div class="form-group">
-            <label class="control-label col-md-2">Tên sản phẩm</label>
-            <input type="text" name="tenSP" class="form-control col-md-10" id="TENSP">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-md-2">Đơn giá</label>
-            <input type="text" name="donGia" class="form-control col-md-10" id="DONGIA">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-md-2">Số lượng</label>
-            <input type="text" name="soLuong" class="form-control col-md-10" id="SOLUONG">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-md-2">Mô tả</label>
-            <input type="text" name="moTa" class="form-control col-md-10" id="MOTA">
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-md-2">Ảnh</label>
-            <div class="col-md-10">
-                <input type="file" name="image" value="Chọn ảnh" id="ANH">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-md-2">Loại sản phẩm</label>
-
-            <select name="MATH" id="MATH" class="form-control col-md-10">
-                <?php
-                $query = "SELECT TENLOAISP FROM loaisanpham;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["TENLOAISP"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-md-2">Thương hiệu</label>
-            <select name="MATH" id="MATH" class="form-control col-md-10">
-                <?php
-                $query = "SELECT TENTHUONGHIEU FROM thuonghieu;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["TENTHUONGHIEU"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-        </div>
-
-
-        <div class="form-group">
-            <label class="control-label col-md">Thông số kĩ thuật</label>
-
-
-            <label class="control-label col-md-10 d-flex">HỆ ĐIỀU HÀNH</label>
-            <select name="HEDIEUHANH" id="HEDIEUHANH" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT HEDIEUHANH FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["HEDIEUHANH"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-            <label class="control-label col-md-10 d-flex">RAM</label>
-            <select name="RAM" id="RAM" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT RAM FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["RAM"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-            <label class="control-label col-md-10 d-flex">ROM</label>
-            <select name="ROM" id="ROM" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT ROM FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["ROM"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-            <label class="control-label col-md-10 d-flex">Kích cỡ màn hình</label>
-            <select name="KICHCOMANHINH" id="KICHCOMANHINH" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT KICHCOMANHINH FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["KICHCOMANHINH"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-            <label class="control-label col-md-10 d-flex">Vy xử lí</label>
-            <select name="VIXULY" id="VIXULY" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT VIXULY FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["VIXULY"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-            <label class="control-label col-md-10 d-flex">PIN</label>
-            <select name="PIN" id="PIN" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT PIN FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["PIN"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-            <label class="control-label col-md-10 d-flex">CAMERA</label>
-            <select name="CAMERA" id="CAMERA" class="form-control col-md-10">
-                <?php
-                $query = "SELECT DISTINCT CAMERA FROM thongsokythuat;";
-                $result = mysqli_query($conn, $query);
-                if (mysqli_num_rows($result) <> 0) {
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<option value=" . ">" . $rows["CAMERA"] . "</option>";
-                    }
-                }
-                ?>
-            </select>
-
-
-
-
-
-        </div>
-
-        <div class="form-group">
-            <div class="col-md-offset-2 col-md-10">
-                <input type="submit" value="Lưu" class="btn btn-primary" />
-            </div>
-        </div>
-
-    </form>
-
-    <?php
-require("../shared/footer.php");
+}
+include("../shared/header.php");
 ?>
-</body>
+<div class="container">
+        <h2 style="text-align:center">Chỉnh sửa</h2>
 
-</html>
+        <form action="" method="post" enctype="multipart/form-data">
+
+                <div class="form-horizontal">
+                        <h4>Sản phẩm</h4>
+                        <hr />
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Mã sản phẩm </label>
+                                <input type="text" class="form-control ml-2" readonly value="<?php echo $maSP ?>"
+                                        name="MASP" style="width:82%">
+                        </div>
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Tên sản phẩm </label>
+                                <div class="col-md-10">
+                                        <input type="text" class="form-control" name="TENSP" required
+                                                value="<?php echo $row['TENSP'] ?>">
+                                </div>
+                        </div>
+
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Đơn giá</label>
+                                <div class="col-md-10">
+                                        <input type="text" class="form-control" name="DONGIA" required
+                                                value="<?php echo $row['DONGIA'] ?>">
+                                </div>
+                        </div>
+
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Số lượng</label>
+                                <div class="col-md-10">
+                                        <input type="number" class="form-control" name="SOLUONG" required
+                                                value="<?php echo $row['SOLUONG'] ?>">
+                                </div>
+                        </div>
+
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Mô tả</label>
+                                <div class="col-md-10">
+                                        <input type="text" class="form-control" name="MOTA" required
+                                                value="<?php echo $row['MOTA'] ?>">
+                                </div>
+                        </div>
+
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Ảnh</label>
+                                <div class="col-md-10">
+                                        <input type="file" value="Chọn File" name="Avatar" accept="image/*" required />
+                                </div>
+                        </div>
+
+                        <?php
+                        $sql_loaisp = "SELECT TENLOAISP, MALOAISP from loaisanpham ";
+                        $result_loaisp = mysqli_query($conn, $sql_loaisp);
+                        ?>
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Loại sản phẩm</label>
+                                <div class="col-md-10">
+                                        <select name="loaisp" id="" class="form-control">
+                                                <?php while ($rows = mysqli_fetch_row($result_loaisp)) {
+                                                        if ($row["TENLOAISP"] == $rows[0]) {
+                                                                echo "<option selected value='$rows[1]'>$rows[0]</option>";
+                                                        } else
+                                                                echo "<option value='$rows[1]'>$rows[0]</option>";
+                                                } ?>
+                                        </select>
+                                </div>
+                        </div>
+
+                        <?php
+                        $sql_thuonghieu = "SELECT TENTHUONGHIEU, MATH from thuonghieu ";
+                        $result_thuonghieu = mysqli_query($conn, $sql_thuonghieu);
+                        ?>
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Thương hiệu</label>
+                                <div class="col-md-10">
+                                        <select name="thuonghieu" id="" class="form-control">
+                                                <?php while ($rows = mysqli_fetch_row($result_thuonghieu)) {
+                                                        if ($row["TENTHUONGHIEU"] == $rows[0]) {
+                                                                echo "<option selected value='$rows[1]'>$rows[0]</option>";
+                                                        } else
+                                                                echo "<option value='$rows[1]'>$rows[0]</option>";
+                                                } ?>
+                                        </select>
+                                </div>
+                        </div>
+
+                        <?php
+                        $sql_tskt = "SELECT MATSKT from thongsokythuat ";
+                        $result_tskt = mysqli_query($conn, $sql_tskt);
+                        ?>
+                        <div class="form-group">
+                                <label class="control-label col-md-2">Thông số kỹ thuật</label>
+                                <div class="col-md-10">
+                                        <select name="tskt" id="" class="form-control">
+                                                <?php while ($rows = mysqli_fetch_row($result_tskt)) {
+                                                        if ($row["MATSKT"] == $rows[0]) {
+                                                                echo "<option selected value='$rows[0]'>$rows[0]</option>";
+                                                        } else
+                                                                echo "<option value='$rows[0]'>$rows[0]</option>";
+                                                } ?>
+                                        </select>
+                                </div>
+                        </div>
+
+                        <div class="form-group">
+                                <div class="col-md-offset-2 col-md-10">
+                                        <input type="submit" value="Lưu" class="btn btn-primary" name="luu" />
+                                </div>
+                        </div>
+                </div>
+        </form>
+
+        <div>
+                <a href="./index.php">Trở về trang danh sách</a>
+        </div>
+</div>
